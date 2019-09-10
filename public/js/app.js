a244fa574d60794e0dca686c5e894925b0352e15
nyacore <bjadmund@mail.ru>
2019-09-10 20:08:47 +0400

    Massive refactor

--- public/js/app.js
+++ public/js/app.js
@@ -2,39 +2,39 @@ import CaesarBaseComponent from "./components/CaesarBaseComponent.js";
 import CaesarBruteForceComponent from "./components/CaesarBruteForceComponent.js";
 
 import {
-  loadJSON,
-  buildViegenereTable
+    loadJSON,
+    buildViegenereTable
 } from "./lib/utils.js";
 
 import {
-  viegenereEncrypt
+    viegenereEncrypt
 } from './lib/algorithms.js';
 
 
 window.onload = async () => {
-  const alphabets = await loadJSON("/public/js/lib/alphabets.json");
-  const russianAlphabet = alphabets["russian"];
-  const englishAlphabet = alphabets["english"];
-  console.log(await viegenereEncrypt('attackatdawn', 'lemon', false, englishAlphabet));
+    const alphabets = await loadJSON("/public/js/lib/alphabets.json");
+    const russianAlphabet = alphabets["russian"];
+    const englishAlphabet = alphabets["english"];
+    console.log(await viegenereEncrypt('attackatdawn', 'lemon', false, englishAlphabet));
 
-  new CaesarBaseComponent(
-    "#caesar-encryption-text",
-    ".caesar-encryption__result",
-    "#caesar-encryption-shift",
-    russianAlphabet
-  );
-  new CaesarBaseComponent(
-    "#caesar-decryption-text",
-    ".caesar-decryption__result",
-    "#caesar-decryption-shift",
-    russianAlphabet,
-    true,
-    true
-  );
+    new CaesarBaseComponent(
+        "#caesar-encryption-text",
+        ".caesar-encryption__result",
+        "#caesar-encryption-shift",
+        russianAlphabet
+    );
+    new CaesarBaseComponent(
+        "#caesar-decryption-text",
+        ".caesar-decryption__result",
+        "#caesar-decryption-shift",
+        russianAlphabet,
+        true,
+        true
+    );
 
-  new CaesarBruteForceComponent(
-    "#caesar-bruteforce-text",
-    ".caesar-bruteforce__result",
-    russianAlphabet
-  );
+    new CaesarBruteForceComponent(
+        "#caesar-bruteforce-text",
+        ".caesar-bruteforce__result",
+        russianAlphabet
+    );
 }
--- public/js/lib/algorithms.js
+++ public/js/lib/algorithms.js
@@ -1,11 +1,7 @@
 import {
-  buildViegenereTable
+    buildViegenereTable
 } from './utils.js';
 
-// import {
-//   russianAlphabet
-// } from "../app.js";
-// const alphabet = russianAlphabet;
 /**
  *
  * @param {String} text Input string
@@ -14,58 +10,58 @@ import {
  * @returns {String} Encrypted string
  */
 export async function caesarEncrypt(
-  text,
-  shift = 0,
-  decrypt = false,
-  alphabet
+    text,
+    shift = 0,
+    decrypt = false,
+    alphabet
 ) {
-  let result = "";
+    let result = "";
 
-  /**
-   * @type {Array<String>}
-   */
+    /**
+     * @type {Array<String>}
+     */
 
-  text.split("").forEach(e => {
-    if ([".", ",", " ", "?", "!"].includes(e)) {
-      result += e;
-    } else {
-      let index;
-      if (decrypt) {
-        index = alphabet.indexOf(e) - (+shift % alphabet.length);
-        if (index < 0) index = alphabet.length + index;
-      } else {
-        index = (alphabet.indexOf(e) + +shift) % alphabet.length;
-      }
-      result += alphabet[index];
-    }
-  });
-  return result;
+    text.split("").forEach(e => {
+        if ([".", ",", " ", "?", "!"].includes(e)) {
+            result += e;
+        } else {
+            let index;
+            if (decrypt) {
+                index = alphabet.indexOf(e) - (+shift % alphabet.length);
+                if (index < 0) index = alphabet.length + index;
+            } else {
+                index = (alphabet.indexOf(e) + +shift) % alphabet.length;
+            }
+            result += alphabet[index];
+        }
+    });
+    return result;
 }
 
 export async function viegenereEncrypt(text, key, decrypt = false, alphabet) {
-  if (key.length > text.length) {
-    return '';
-  }
-
-  const table = await buildViegenereTable(alphabet);
-
-  let index = 0;
-  let newKey = key;
-  while (newKey.length < text.length) {
-    newKey += key[index++ % key.length];
-  }
-
-  let result = '';
-  newKey.split('').forEach((e, index) => {
-    const keyIndex = alphabet.indexOf(e);
-    const inputIndex = alphabet.indexOf(text[index]);
-
-    if (decrypt) {
-      result += table[keyIndex][inputIndex];
-    } else {
-      result +=
+    if (key.length > text.length) {
+        return '';
     }
-  });
 
-  return result;
+    const table = await buildViegenereTable(alphabet);
+
+    let index = 0;
+    let newKey = key;
+    while (newKey.length < text.length) {
+        newKey += key[index++ % key.length];
+    }
+
+    let result = '';
+    newKey.split('').forEach((e, index) => {
+        const keyIndex = alphabet.indexOf(e);
+        const inputIndex = alphabet.indexOf(text[index]);
+
+        if (decrypt) {
+            result += table[keyIndex][inputIndex];
+        } else {
+            // result +=
+        }
+    });
+
+    return result;
 }
